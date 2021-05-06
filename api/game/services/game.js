d328107e631fdb384a1f114e63d1a7f6ae620405
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-05-06 19:56:23 -0300

    Adicionado uma nova rota POST populate para game, iniciado a lógica no service de game para obter os dados dos games

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,4 +5,52 @@
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+const slugify = require("slugify");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const ratingElement = dom.window.document.querySelector(".age-restrictions__icon use");
+
+  const description = dom.window.document.querySelector(".description");
+
+  console.log(description)
+
+  return {
+    rating: ratingElement ? ratingElement.getAttribute("xlink:href").replace(/_/g, "").replace(/[^\w-]+/g, "") : "BR0",
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if(!item) {
+    await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true })
+    });
+  }
+}
+
+module.exports = {
+  populate: async (params) => {
+
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const { data: { products } } = await axios.get(gogApiUrl);
+
+    await create(products[0].publisher, "publisher");
+    await create(products[0].developer, "developer");
+  }
+};
