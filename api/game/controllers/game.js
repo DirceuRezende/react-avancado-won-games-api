a269f5fb774f7c9d1f6ab8453fb588578917db6d
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-05-07 19:55:02 -0300

    Finalizado a rota e serviço de popular os dados no strapi

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -7,7 +7,12 @@
 
 module.exports = {
   populate: async (ctx) => {
-    await strapi.services.game.populate();
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+    await strapi.services.game.populate(options);
     ctx.send({ ok: true });
   }
 };
