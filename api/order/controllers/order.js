22f03025a6e4b39ede25898d984239e6b2096395
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-09-24 20:00:46 -0300

    Add Order Collection

---
+++ api/order/controllers/order.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+ module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id,
+        });
+
+        if (validatedGame) {
+          games.push(validatedGame);
+        }
+      })
+    );
+
+    if (!games.length) {
+      ctx.response.status = 404;
+      return {
+        error: "No valid games found!",
+      };
+    }
+
+    const total = games.reduce((acc, game) => {
+      return acc + game.price;
+    }, 0);
+
+    if (total === 0) {
+      return {
+        freeGames: true,
+      };
+    }
+
+    return { total_in_cents: total * 100, games };
+  }
+};
